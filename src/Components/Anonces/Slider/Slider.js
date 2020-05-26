1cbf6d7f8a2948a519f3a09b0ea45daa8904c611
Liubomyr <43443223+LiubomyrChereshchuk@users.noreply.github.com>
2020-05-27 01:18:06 +0300

    Adaptive
    Slider adaptive. Main page adaptive done.

--- src/Components/Anonces/Slider/Slider.js
+++ src/Components/Anonces/Slider/Slider.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Poster from '../../Poster/Poster';
 import './Slider.css';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { AddFilmCreator } from '../../../redux/moviePage-reducer';
 
 import SliderArrow from './SliderArrow/SliderArrow';
@@ -18,14 +16,55 @@ export default class MultipleItems extends Component {
 
     render() {
         const settings = {
-            autoplay: false,
+            autoplay: true,
             dots: false,
             infinite: true,
-            speed: 500,
+            speed: 1000,
             slidesToShow: 5,
             slidesToScroll: 1,
             prevArrow: <SliderArrow to="prev" side='left' arr='arrowprev' />,
             nextArrow: <SliderArrow to="next" side='right' stl='rotate(180deg)' arr='arrownext' />,
+            responsive: [
+                {
+                    breakpoint: 1500,
+                    settings: {
+                        slidesToShow: 4,
+                    }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 980,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 750,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 570,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: true,
+                    }
+                },
+                {
+                    breakpoint: 400,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                    }
+                }
+            ]
         };
 
         let items = Array.from(this.props.announcesFilms);
@@ -64,7 +103,7 @@ export default class MultipleItems extends Component {
             )
         })
         return (
-            <div>
+            <div >
                 {itemsToRender.length < 6 ? (
                     <div style={{ display: 'flex' }}>
                         {itemsToRender}
