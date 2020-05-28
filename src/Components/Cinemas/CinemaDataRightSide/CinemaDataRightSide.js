f91ed8c7f84bb104a1d943ac374357f99c017cc9
Liubomyr <43443223+LiubomyrChereshchuk@users.noreply.github.com>
2020-05-28 11:51:35 +0300

    Scroll to cinema_info

--- src/Components/Cinemas/CinemaDataRightSide/CinemaDataRightSide.js
+++ src/Components/Cinemas/CinemaDataRightSide/CinemaDataRightSide.js
@@ -5,11 +5,17 @@ import CinemaDescription from './CinemaDescription/CinemaDescription';
 
 
 class CinemaDataRightSide extends React.Component {
+    myRef = React.createRef();
 
+    componentDidMount() {
+        let el = this.myRef.current;
+        el.scrollIntoView({ behavior: "smooth", });
+
+    }
 
     render() {
         return (
-            <div className="cinema__rightside">
+            <div ref={this.myRef} className="cinema__rightside">
                 <CinemaDescription
                     cinemaData={this.props.cinemaData}
                 />
